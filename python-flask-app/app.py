bf4812b6082066b2351972b47c18807fd9809f3b
Hermen Nicolau <hnicolau@hnicolau5MD6R.vmware.com>
2022-12-26 12:07:42 +0000

    adding comment to the app.py

--- python-flask-app/app.py
+++ python-flask-app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 import os
 
+# This is not the original app, this is the app.py after running the script. 
 @app.route('/')
 def hello_world():
     return 'Hello, GitGuardian!'
